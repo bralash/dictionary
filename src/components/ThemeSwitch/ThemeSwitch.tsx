504134bdc792a6d46116556404dba2306fae4884
bralash <emmanuelasaber@gmail.com>
2023-04-08 21:26:35 +0000

    Feature: Complete theme implementation with localStorage and browser default

--- src/components/ThemeSwitch/ThemeSwitch.tsx
+++ src/components/ThemeSwitch/ThemeSwitch.tsx
@@ -1,18 +1,27 @@
 import * as React from "react";
-import './ThemeSwitch.css'
-import { BiMoon } from 'react-icons/bi'
+import "./ThemeSwitch.scss";
+import { BiMoon } from "react-icons/bi";
+import { useContext } from "react";
+import { ThemeContext } from "../../contexts/theme-context";
 
 interface IThemeSwitchProps {}
 
 const ThemeSwitch: React.FunctionComponent<IThemeSwitchProps> = (props) => {
+  const { theme, setTheme } = useContext(ThemeContext);
+
+  const handleThemeSwitch = () => {
+    const isCurrentDark = theme === 'dark';
+    setTheme(isCurrentDark ? 'light' : 'dark');
+    localStorage.setItem('theme', isCurrentDark ? 'light' : 'dark');
+  };
+  
   return (
     <div className="theme">
       <label className="switch">
-        <input type="checkbox" title="Theme" />
+        <input type="checkbox" title="Theme" onChange={handleThemeSwitch} checked={theme === 'dark'} />
         <span className="slider round"></span>
       </label>
-
-      <BiMoon size={20} />
+      <BiMoon size={20} color={theme === 'dark' ? '#A445ED': '#757575'}/>
     </div>
   );
 };
